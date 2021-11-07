ee65cef508572a9fd5b75ab607b8fc968cadabb9
elizaveta <elizavetaBun@gmail.com>
2021-11-07 19:31:37 +0400

    add dynamically loaded manu to each restaurant

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,31 @@
 const restaurant = 'food-band';
+const cardsMenu = document.querySelector('.cards-menu');
 
 const renderItems = (data) => {
-  data.forEach((element, index) => {
-    console.log(data[index]);
+  data.forEach(({ name, description, price, image }) => {
+    const div = document.createElement('div');
+
+    div.classList.add('card');
+
+    div.innerHTML = `
+    <img src="${image}" alt="image" class="card-image" />
+    <div class="card-text">
+      <div class="card-heading">
+        <h3 class="card-title card-title-reg">${name}</h3>
+      </div>
+      <div class="card-info">
+        <div class="ingredients">${description}</div>
+      </div>
+      <div class="card-buttons">
+        <button class="button button-primary button-add-cart">
+          <span class="button-card-text">В корзину</span>
+          <span class="button-cart-svg"></span>
+        </button>
+        <strong class="card-price-bold">${price} ₽</strong>
+      </div>
+      `;
+
+    cardsMenu.append(div);
   });
 };
 
